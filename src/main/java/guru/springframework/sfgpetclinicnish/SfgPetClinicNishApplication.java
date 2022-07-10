475cb5cd0d5dc28c91a29dd849129b39209b32cc
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-07-10 14:02:32 +0800

    Initial Project Add

---
+++ src/main/java/guru/springframework/sfgpetclinicnish/SfgPetClinicNishApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinicnish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicNishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinicNishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfgpetclinicnish/SfgPetClinicNishApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinicnish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicNishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
