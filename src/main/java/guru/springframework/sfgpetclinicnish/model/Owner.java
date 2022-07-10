0d169da30a4ed5c3b523b0f494e4b64c58071557
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-07-10 20:23:27 +0800

    adding POJOS. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinicnish/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgpetclinicnish.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/guru/springframework/sfgpetclinicnish/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgpetclinicnish.model;
+
+public class Vet extends Person{
+}
